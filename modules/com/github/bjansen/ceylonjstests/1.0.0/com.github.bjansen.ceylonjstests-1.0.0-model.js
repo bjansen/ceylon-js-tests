5397776c706a0a562b48f7c8f6b6429f2af5849b
Bastien Jansen <bastien.jansen@gmx.com>
2016-12-29 13:56:00 +0100

    test

---
+++ modules/com/github/bjansen/ceylonjstests/1.0.0/com.github.bjansen.ceylonjstests-1.0.0-model.js
@@ -0,0 +1,7 @@
+(function(define) { define(function(require, ex$, module) {
+ex$.$CCMM$={"$mod-nat":["js"],"$mod-version":"1.0.0","$mod-deps":["ceylon.language\/1.3.2-SNAPSHOT","ceylon.interop.browser\/1.3.2-SNAPSHOT"],"com.github.bjansen.ceylonjstests":{"$pkg-pa":1,runInteropChecks:{$t:{md:"$",pk:"$",nm:"Anything"},$ff:1,mt:"m",nm:"runInteropChecks"},run:{$t:{md:"$",pk:"$",nm:"Anything"},pa:1,$ff:1,mt:"m",an:[{doc:["Run the module `com.github.bjansen.ceylonjstests`."]}],nm:"run"},check:{$t:{md:"$",pk:"$",nm:"Anything"},ps:[[{$t:{md:"$",pk:"$",nm:"String"},mt:"prm",nm:"description"},{$t:{md:"$",pk:"$",ta:{"Callable.Arguments":{md:"$",pk:"$",nm:"Empty"},"Callable.Return":{md:"$",pk:"$",nm:"Anything"}},nm:"Callable"},mt:"prm",an:[{doc:["Returning false means an assertion failed"]}],nm:"test"}]],$ff:1,mt:"m",nm:"check"},"$pkg-anns":[{doc:["Default documentation for package `com.github.bjansen.ceylonjstests`."]}]},"$mod-bin":"9.2","$mod-anns":[{doc:["Default documentation for module `com.github.bjansen.ceylonjstests`."]},{native:["js"]}],"$mod-name":"com.github.bjansen.ceylonjstests"};
+});
+}(typeof define==='function' && define.amd ? define : function (factory) {
+if (typeof exports!=='undefined') { factory(require, exports, module);
+} else { throw 'no module loader'; }
+}));
